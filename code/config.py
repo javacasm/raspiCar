dff6f279f29b1732dc12890940b90269fcdb5ab6
javacasm <javacasm@gmail.com>
2020-07-20 17:23:58 +0200

    add

---
+++ code/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Configuration file
+    Licencia CC by @javacasm    
+    Julio de 2020
+"""
+
+
+# Telegram Bot Authorization Token
+
+v = '1.0'
+
+TELEGRAM_API_TOKEN = 'PON TU TOKEN AQUI'
+
+ImagesDirectory = './images/'
+ImagesPartition = 'root'
+
+# chat_id del Admin
+ADMIN_USER =  123123
+
+ALLOWED_USERS = [ADMIN_USER]
