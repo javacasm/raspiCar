3545255b8525f24d704d6b47557818c65a68868a
javacasm <javacasm@gmail.com>
2020-07-22 13:30:34 +0200

    update

--- code/test_robot.py
+++ code/test_robot.py
@@ -1,14 +1,14 @@
 import robot
 import time
 
-espera = 5 
+espera = 2 
 
 robot.init()
 for i in range(0,4):
-    robot.forward(robot.throttle_med)
+    robot.forward(robot.throttle_max)
     time.sleep(espera)
-    robot.right(robot.throttle_med)
-    time.sleep(espera)
+    robot.right(robot.throttle_max)
+    time.sleep(espera/2)
 robot.stop()
 
 
