042b74dba3c0a8a77b7ae79c687c5a50c978422c
javacasm <javacasm@gmail.com>
2020-07-23 17:03:34 +0200

    update

--- code/robotApp.py
+++ code/robotApp.py
@@ -3,14 +3,32 @@ from flask import Flask, render_template, request, Response
 import robot
 
 import time
+import smbus2
+import bme280
+
+port = 1
+address = 0x76 # usaremos la dirección que hemos encontrado
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address) # parámetros de compensación
 
 app = Flask('Robot control')
+temperature  = 0
+presion = 0
+humedad = 0
+fechaDatos = ''
 state_msg = 'Hola desde raspiCar '
 robot.init()
 
 @app.route('/')
 def index():
-   return render_template('./index.html', state_msg=state_msg)
+   data = bme280.sample(bus, address, calibration_params) 
+
+   fechaDatos = data.timestamp
+   temperatura = int(data.temperature)
+   presion = int(data.pressure)
+   humedad = int(data.humidity)
+   return render_template('./index.html', state_msg=state_msg, presion = presion, humedad = humedad, temperatura = temperatura, fechaDatos = fechaDatos)
 
 @app.route('/move/<direction>')
 def move(direction):
