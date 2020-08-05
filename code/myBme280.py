2cea535cb538e7c68f959d0a1c5e2cec96738967
javacasm <javacasm@gmail.com>
2020-08-05 21:12:25 +0200

    update

--- code/myBme280.py
+++ code/myBme280.py
@@ -13,26 +13,37 @@ import bme280
 
 import utils
 
+v = '0.5'
+
 port = 1
 address = 0x76 # usaremos la dirección que hemos encontrado
 
 bus = None
 calibration_params = None
 
+
 def init():
     global bus
     global calibration_params
+    global bInit
 
     bus = smbus2.SMBus(port)
 
     calibration_params = bme280.load_calibration_params(bus, address) # parámetros de compensación
 
+def getStrData():
+    temperature, pressure, humidity, timestamp, id = getData()
+    return 'Temp:{}º Pres:{} mb Hum:{}% at {} id: {}'.format(int(temperature), int(pressure), int(humidity), timestamp, id)
+
+
 def getData():
     global bus
     global calibration_params
 
+    if calibration_params == None:
+        init()
 # leemos los datos
-    data = bme280.sample(bus, address, calibration_params) 
+    data = bme280.sample(bus, address, calibration_params)
 
 # los mostramos en otro formato
     utils.myLog(str(data))
